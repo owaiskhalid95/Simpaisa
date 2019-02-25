7860a1854d3391e87685491559e5b6d49ad60eae
tali <tarufali@hotmail.com>
2019-02-26 02:48:02 +0500

    Initial commit

---
+++ test/com/simpaisa/workflows/general/WifiTransactionThreshold.java
@@ -0,0 +1,71 @@
+package com.simpaisa.workflows.general;
+
+import com.simpaisa.utilities.SimpaisaAPIRequests;
+import com.simpaisa.utilities.SimpaisaHashMapper;
+import com.simpaisa.utilities.SimpaisaResponseParser;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class WifiTransactionThreshold {
+    @Test(enabled = true, groups = {"wifi","workflow"})
+    public void make_transasction_wifi() throws IOException, URISyntaxException {
+
+        //Note: Threshold applied, sufficient balance , Product, Operator, mobile all in place(properties)
+        // Out of scope : Mobile messages , mobile balance
+
+
+        SimpaisaAPIRequests request1 = new SimpaisaAPIRequests();
+        System.out.println(" STEP01: === Initiating Make Transaction Request (POST) ");
+        SimpaisaResponseParser response1 = request1.wifi_make_transaction();
+
+        SimpaisaHashMapper output1 = new SimpaisaHashMapper(response1.misc1());
+        System.out.println("  < Display Request Responses >");
+
+        System.out.println("Response Body Message : " + output1.getValue("message"));
+        Assert.assertEquals(output1.getValue("message"), "The transaction is in PIN request status");
+
+        SimpaisaAPIRequests request2 = new SimpaisaAPIRequests();
+        System.out.println(" STEP02: === Initiating Get OTP Request (GET)");
+        String response2 = request2.wifi_get_OTP();
+        System.out.println("  < Display Request Responses >");
+        System.out.println("OTP COde : " + response2);
+
+        System.out.println(" STEP03: === Initiating Verify Transaction Request (POST) ");
+        SimpaisaAPIRequests request3 = new SimpaisaAPIRequests();
+        SimpaisaResponseParser response3 = request3.wifi_verify_payment(response2);
+
+        SimpaisaHashMapper output3 = new SimpaisaHashMapper(response3.misc1());
+
+        System.out.println("  < Display Request Responses >");
+
+        System.out.println("Response Body Message : " + output3.getValue("message"));
+        Assert.assertEquals(output3.getValue("message"), "0-Transaction Successful");
+
+        //TODO: Tentry in ransaction and recursion table must be = 1
+
+
+        // TODO:  OUT OF SCOPE: unsubscribe user
+
+        // TODO: repeat STep 01 repeated , we'll get response "Transaction periodic threshold exceeded"
+
+        SimpaisaAPIRequests request4 = new SimpaisaAPIRequests();
+        System.out.println(" STEP01: === Initiating Make Transaction Request (POST) ");
+        SimpaisaResponseParser respons41 = request4.wifi_make_transaction();
+
+        SimpaisaHashMapper output4 = new SimpaisaHashMapper(response1.misc1());
+        System.out.println("  < Display Request Responses >");
+
+        System.out.println("Response Body Message : " + output1.getValue("message"));
+        Assert.assertEquals(output4.getValue("message"), "Transaction periodic threshold exceeded");
+
+
+
+
+
+    }
+
+
+}
